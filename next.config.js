0ea931e25ea58f49b7d0fbcb176f7cb929b6bce6
Tom Maguire <Tomrmx18@gmail.com>
2023-12-17 23:21:23 +1100

    changing output mode

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ import { withContentlayer } from 'next-contentlayer';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
 };
 
 export default withContentlayer(nextConfig);
